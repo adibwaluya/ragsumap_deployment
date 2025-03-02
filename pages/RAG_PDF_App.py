97afae1ff3a4ddd7c58f7bd27e7e3c2fbc8eac42
Adib Ghassani Waluya <s0567271@htw-berlin.de>
2025-03-02 21:16:05 +0100

    fixing the lib error desperately

--- pages/RAG_PDF_App.py
+++ pages/RAG_PDF_App.py
@@ -12,10 +12,11 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 import os
 from dotenv import load_dotenv
-""" import pysqlite3 as sqlite3
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3') """
+# """ import pysqlite3 as sqlite3
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3') """
+import sqlite3
 
 load_dotenv()
 
